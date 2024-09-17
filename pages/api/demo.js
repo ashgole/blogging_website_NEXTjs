ed8faa5e2e848b2e1fec59e8ccf7d7cc8b9c9c23
ashabb <ashishgole99955@gmail.com>
2024-09-17 12:15:41 +0530

    u

--- pages/api/demo.js
+++ pages/api/demo.js
@@ -2,9 +2,14 @@
 import { NextResponse } from 'next/server';
 
 export const config = {
-  runtime: 'edge', // Specifies that this route should use the Edge runtime
+    runtime: 'edge', // Specifies that this route should use the Edge runtime
 };
 
 export default async function handler() {
-  return NextResponse.json({ message: 'Hello, World!' });
+    return NextResponse.json({ message: 'Hello, World!' },
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        }
+    );
 }
--- pages/api/getallblogs.js
+++ pages/api/getallblogs.js
@@ -1,24 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 
-import * as fs from 'fs';
+import fs from 'fs';
+import path from 'path';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');  // Adjust for your security needs
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-
   if (req.method === 'GET') {
-    let data = await fs.promises.readdir("./static/json/");
-    let myFile;
-    let allBlogs = [];
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i]
-      myFile = await fs.promises.readFile(('./static/json/' + item), 'utf-8')
-      allBlogs.push(JSON.parse(myFile))
+    try {
+      const dirPath = path.join(process.cwd(), 'static/json/');
+      const data = await fs.promises.readdir(dirPath);
+      let allBlogs = [];
+
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const filePath = path.join(dirPath, item);
+        const myFile = await fs.promises.readFile(filePath, 'utf-8');
+        allBlogs.push(JSON.parse(myFile));
+      }
+
+      res.status(200).json(allBlogs);
+    } catch (error) {
+      res.status(500).json({ message: 'Error reading files', error });
     }
-    res.status(200).json(allBlogs)
   } else {
     res.status(405).json({ message: 'Method not allowed' });
-    return
   }
 }
