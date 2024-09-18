cc03388dfe61258adfa7a1398e98713ab1fbee92
ashabb <ashishgole99955@gmail.com>
2024-09-18 13:37:37 +0530

    u

--- pages/contact/ContactsPage.js
+++ pages/contact/ContactsPage.js
@@ -6,21 +6,21 @@ import { rootPath } from '@/utils/constants';
 const ContactsPage = () => {
   const [contacts, setContacts] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchContacts = async () => {
-  //     const response = await axios.get(`${rootPath}/api/contact/getContact`);
-  //     setContacts(response.data);
-  //   };
-  //   fetchContacts();
-  // }, []);
+  useEffect(() => {
+    const fetchContacts = async () => {
+      const response = await axios.get(`${rootPath}/api/contact/getContact`);
+      setContacts(response.data);
+    };
+    fetchContacts();
+  }, []);
 
 
 
   return (
     <div className="container mx-auto">
       ash
-      {/* <h1 className="text-3xl font-bold text-center my-8">Contact List</h1>
-      <ContactList contacts={contacts} setContacts={setContacts} /> */}
+      <h1 className="text-3xl font-bold text-center my-8">Contact List</h1>
+      {/* <ContactList contacts={contacts} setContacts={setContacts} /> */}
     </div>
   );
 };
