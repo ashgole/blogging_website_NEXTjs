9fa06b1d3ab9eeb484afa935a9899b155cdefaeb
ashabb <ashishgole99955@gmail.com>
2024-09-18 13:41:29 +0530

    u

--- pages/contact/ContactsPage.js
+++ pages/contact/ContactsPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ContactList from './ContactList';
+// import ContactList from './ContactList';
 import axios from 'axios';
 import { rootPath } from '@/utils/constants';
 
@@ -20,7 +20,7 @@ const ContactsPage = () => {
     <div className="container mx-auto">
       ash
       <h1 className="text-3xl font-bold text-center my-8">Contact List</h1>
-      <ContactList contacts={contacts} setContacts={setContacts} />
+      {/* <ContactList contacts={contacts} setContacts={setContacts} /> */}
     </div>
   );
 };
