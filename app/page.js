c438b996638beea06d183800fc5633cf252d2a76
ashabb <ashishgole99955@gmail.com>
2024-09-24 21:45:57 +0530

    u

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 
 import AddBlog from "./components/addBlog/AddBlog";
-// import Blog from "./pages/blog/page";
+import Blog from "./pages/blog/page";
 
 export default function Home() {
   return (
     <>
        <AddBlog />
-       {/*<Blog /> */}
+        <Blog />
     </>
   );
 }
