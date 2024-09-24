218505a039a2f33fcec85251ef1a494efe4b47fc
ashabb <ashishgole99955@gmail.com>
2024-09-24 21:42:37 +0530

    removed unwanted

--- components/dangerouslySetInnerHTML/DangerouslySetInnerHTML.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const DangerouslySetInnerHTML = ({content}) => {
-    const markup = { __html: content }
-    return <div dangerouslySetInnerHTML={markup} />;
-}
-
-export default DangerouslySetInnerHTML
